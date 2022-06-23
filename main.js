3acd58f3db5c018e028e9b6ae9ba5d3c613cfc30
Vishnu Kyatannawar <vishnukyatannawar@gmail.com>
2022-06-23 18:20:06 +0530

    Navigate to most recent posts

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const password = argv[3];
     // Allow user to see what is happening on the browser
     launchOptions.headless = false;
     // slow down by 350ms
-    launchOptions.slowMo = 350;
+    launchOptions.slowMo = 100;
   }
 
   // Create a browser instance
@@ -30,13 +30,22 @@ const password = argv[3];
   }  
 
   await page.goto(pageLink, { waitUntil: 'networkidle0' });
+  await helper.delay(5000);
   // Sometimes noticed that instead of sign in we see join now page
   // If that happens we can use the below line of code to redirect to sign in page
   // await page.click('[data-tracking-control-name="auth_wall_desktop_company-login-toggle"]');
   await page.type('#username', username);
   await page.type('#password', password);
-  await page.click('button[type=submit]'); 
+  await page.click('button[type=submit]');
   
-  await helper.delay(10000);
+  // Wait until the company post page loads 
+  await page.waitForSelector('button[data-control-name="feed_sort_dropdown_trigger"]', { visible: true, timeout: 0 });
+  
+  // Click on sort dropdown and click on recent posts
+  await page.click('button[data-control-name="feed_sort_dropdown_trigger"]');
+  const sortDropDown = await page.$x('//button[contains(., "Recent")]');
+  await sortDropDown[0].click();
+
+  await helper.delay(5000);
   await browser.close();
 })();
